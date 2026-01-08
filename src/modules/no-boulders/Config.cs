b9ec67bc344a2d87aa918afe2e88dfb7b56a614a
Kaden5480 <kaden5480@protonmail.com>
2026-01-08 01:09:08 +0000

    Implement no boulders

---
+++ src/modules/no-boulders/Config.cs
@@ -0,0 +1,28 @@
+using BepInEx.Configuration;
+using ModMenu.Config;
+
+namespace SpeedrunMod.Modules.NoBoulders {
+    /**
+     * <summary>
+     * The config for this module.
+     * </summary>
+     */
+    internal static class Config {
+        [Field("Enabled")]
+        internal static ConfigEntry<bool> enabled;
+
+        /**
+         * <summary>
+         * Initializes the config, binding to the provided
+         * config file.
+         * </summary>
+         * <param name="configFile">The config file to bind to</param>
+         */
+        internal static void Init(ConfigFile configFile) {
+            enabled = configFile.Bind(
+                Module.name, "enabled", true,
+                $"Whether {Module.name} is enabled"
+            );
+        }
+    }
+}
---
+++ src/modules/no-boulders/Module.cs
@@ -0,0 +1,48 @@
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace SpeedrunMod.Modules.NoBoulders {
+    /**
+     * <summary>
+     * Prevents boulders from respawning
+     * on Ugsome Stórr while this module
+     * is enabled.
+     * </summary>
+     */
+    [HarmonyPatch(typeof(FallingRock), "InitialiseRock")]
+    internal static class DisableBoulders {
+        private static bool Prefix() {
+            if (Config.enabled.Value == true) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    /**
+     * <summary>
+     * The main module for No Boulders.
+     * </summary>
+     */
+    internal static class Module {
+        /**
+         * <summary>
+         * The name of this module.
+         * </summary>
+         */
+        internal const string name = "No Boulders";
+
+        /**
+         * <summary>
+         * Initializes this module, binding its config to the
+         * provided config file.
+         * </summary>
+         * <param name="configFile">The config file to bind to</param>
+         */
+        internal static void Init(ConfigFile configFile) {
+            Config.Init(configFile);
+            Patcher.Patch<DisableBoulders>();
+        }
+    }
+}
