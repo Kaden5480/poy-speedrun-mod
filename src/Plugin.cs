950cf19529de77c5c6a34deba928f84cc829ca27
Kaden5480 <kaden5480@protonmail.com>
2026-09-13 13:39:57 +0100

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Linq;
+
+using BepInEx;
+using HarmonyLib;
+using ModMenu;
+
+namespace SpeedrunMod {
+    [BepInDependency("com.github.Kaden5480.poy-ui-lib")]
+    [BepInDependency(
+        "com.github.Kaden5480.poy-mod-menu",
+        BepInDependency.DependencyFlags.SoftDependency
+    )]
+    [BepInPlugin("com.github.Kaden5480.poy-speedrun-mod", "SpeedrunMod", PluginInfo.PLUGIN_VERSION)]
+    internal class Plugin : BaseUnityPlugin {
+        private static Plugin instance;
+
+        /**
+         * <summary>
+         * Executes when the plugin is being loaded.
+         * </summary>
+         */
+        private void Awake() {
+            instance = this;
+
+            // Register with Mod Menu as an optional dependency
+            if (AccessTools.AllAssemblies().FirstOrDefault(
+                    a => a.GetName().Name == "ModMenu"
+                ) != null
+            ) {
+                Register();
+            }
+        }
+
+        /**
+         * <summary>
+         * Registers with Mod Menu.
+         * </summary>
+         */
+        private void Register() {
+            ModInfo info = ModManager.Register(this);
+            info.license = "GPL-3.0";
+        }
+
+        /**
+         * <summary>
+         * Logs a debug message.
+         * </summary>
+         * <param name="message">The message to log</param>
+         */
+        internal static void LogDebug(string message) {
+#if DEBUG
+            if (instance == null) {
+                Console.WriteLine($"[Debug] SpeedrunMod: {message}");
+                return;
+            }
+
+            instance.Logger.LogInfo(message);
+#else
+            if (instance != null) {
+                instance.Logger.LogDebug(message);
+            }
+#endif
+        }
+
+        /**
+         * <summary>
+         * Logs an informational message.
+         * </summary>
+         * <param name="message">The message to log</param>
+         */
+        internal static void LogInfo(string message) {
+            if (instance == null) {
+                Console.WriteLine($"[Info] SpeedrunMod: {message}");
+                return;
+            }
+            instance.Logger.LogInfo(message);
+        }
+
+        /**
+         * <summary>
+         * Logs an error message.
+         * </summary>
+         * <param name="message">The message to log</param>
+         */
+        internal static void LogError(string message) {
+            if (instance == null) {
+                Console.WriteLine($"[Error] SpeedrunMod: {message}");
+                return;
+            }
+            instance.Logger.LogError(message);
+        }
+    }
+}
